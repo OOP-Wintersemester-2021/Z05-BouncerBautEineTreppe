c26eb5a6c9634a0028722655322d25e1a8a6db5c
Alexander Bazo <alexanderbazo@googlemail.com>
2021-10-31 23:45:33 +0100

    Add comments

--- src/BouncerBautEineTreppe.java
+++ src/BouncerBautEineTreppe.java
@@ -2,6 +2,13 @@ import de.ur.mi.bouncer.apps.BouncerApp;
 import de.ur.mi.bouncer.apps.BouncerLauncher;
 
 
+/*
+ * In diesem Programm soll Bouncer am östlichen Ende der Karte, ganz unten am Kartenrand,
+ * eine blaue Treppe bauen. Die Treppe soll fünf Felder hoch sein. Jede Stufe ist genau ein
+ * Feld breit. Bouncer soll ganz am Ende zu seiner Ausgangsposition zurückkehren. Alle Arbeits-
+ * schritte, die Bouncer für das Bauen der Treppe mehr als einmal durchführen muss, sollen,
+ * mit entsprechenden Parametern, als Methoden abgebildet werden.
+ */
 public class BouncerBautEineTreppe extends BouncerApp {
 
     @Override
